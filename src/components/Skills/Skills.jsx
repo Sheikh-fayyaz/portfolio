5a619773687067725ea6622b4527c9fcc57de5d2
sheikh fayyaz <fayyaz@zirafftechnologies.com>
2025-01-29 13:12:00 +0530

    image path

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -9,11 +9,8 @@ export default function Skills() {
           <div className="col-md-2 col-4" key={id}>
             <div className="skill-card text-center mb-4">
               <div className="skill-card-inner primary-bg p-md-4 p-3 rounded-4 mb-3">
-                <img
-                  src={logo}
-                  className="d-block mx-auto mb-md-4 mb-3"
-                  alt={title}
-                />
+              <img src={`${import.meta.env.BASE_URL}${logo}`} className="d-block mx-auto mb-md-4 mb-3" alt={title} />
+
                 <small className="number opacity-75">{number}</small>
               </div>
               <p className="d-md-block d-none">{title}</p>
