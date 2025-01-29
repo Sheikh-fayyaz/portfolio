779d46a9e764d2edb4cfa1086de9f659210fbdff
sheikh fayyaz <fayyaz@zirafftechnologies.com>
2025-01-29 12:38:37 +0530

    added imagdata on component

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,10 +1,79 @@
 import "./Skills.css";
-import SKillsData from "../../Data/Skills.json";
 
 export default function Skills() {
+  const imgData = [
+    {
+      "id": "1",
+      "title": "HTML5",
+      "logo": "src/assets/images/icons/html5.svg",
+      "number": "92%"
+    },
+    {
+        "id": "2",
+        "title": "CSS3",
+        "logo": "src/assets/images/icons/css3.svg",
+        "number": "90%"
+      },
+      {
+        "id": "3",
+        "title": "SASS",
+        "logo": "src/assets/images/icons/sass.svg",
+        "number": "85%"
+      },
+      {
+        "id": "4",
+        "title": "Bootstrap 5",
+        "logo": "src/assets/images/icons/bootstrap5.svg",
+        "number": "90%"
+      },
+      {
+        "id": "5",
+        "title": "Tailwind",
+        "logo": "src/assets/images/icons/tailwind.svg",
+        "number": "90%"
+      },
+      {
+        "id": "6",
+        "title": "JavaScript",
+        "logo": "src/assets/images/icons/javascript.svg",
+        "number": "85%"
+      },
+      {
+        "id": "7",
+        "title": "Jquery",
+        "logo": "src/assets/images/icons/jquery2.svg",
+        "number": "80%"
+      },
+      {
+        "id": "8",
+        "title": "ReactJS",
+        "logo": "src/assets/images/icons/react.svg",
+        "number": "85%"
+      },
+      {
+        "id": "9",
+        "title": "HubSpot",
+        "logo": "src/assets/images/icons/hubspot-1.svg",
+        "number": "90%"
+      },
+      {
+        "id": "10",
+        "title": "Figma",
+        "logo": "src/assets/images/icons/figma.svg",
+        "number": "70%"
+      },
+      {
+        "id": "11",
+        "title": "Photoshop",
+        "logo": "src/assets/images/icons/photoshop.svg",
+        "number": "75%"
+      }
+]
+
+  
   return (
     <div className="row justify-content-center">
-      {SKillsData.map(({ id, title, logo, number }) => {
+      {imgData.map(({ id, title, logo, number }) => {
         return (
           <div className="col-md-2 col-4" key={id}>
             <div className="skill-card text-center mb-4">
