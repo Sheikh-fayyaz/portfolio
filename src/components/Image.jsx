6930ee4d9f2ca6e86d2d536484e21c4b287a04ff
Sheikh-Fayyaz-OdinSchool <fayyaz@odinschool.com>
2025-01-29 16:31:59 +0530

    First Fold Image fised

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,5 +1,5 @@
 export default function Image({imgPath, imgAlt, imgLoading, imgfetchpriority}) {
   return (
-    <img src={imgPath} className="w-100 rounded-4" alt={imgAlt} loading={imgLoading} fetchpriority={imgfetchpriority} />
+    <img src={imgPath} className="w-100 rounded-4 first-fold-img" alt={imgAlt} loading={imgLoading} fetchpriority={imgfetchpriority} />
   )
 }
