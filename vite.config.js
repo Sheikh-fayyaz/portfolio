0f63417f83d634c9f57f176989f71af2ee699278
Sheikh-Fayyaz-OdinSchool <fayyaz@odinschool.com>
2025-01-29 17:21:56 +0530

    added purgecss

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import WindiCSS from 'vite-plugin-windicss';
+import PurgeCSS from 'vite-plugin-purgecss';
 
 export default defineConfig({
-  plugins: [react(), WindiCSS()],
-  base: '/portfolio/', // Use your GitHub repo name
+  plugins: [react(), WindiCSS(), PurgeCSS()],
+  base: '/portfolio/', // Ensure it's your GitHub repo name
+  build: {
+    minify: 'esbuild',
+    cssCodeSplit: true, // Splits CSS into smaller files
+  },
 });
