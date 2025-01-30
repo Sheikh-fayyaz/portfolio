c61d58245ccc4aa7ebc7ab34e30a7f5dc044629b
Sheikh-Fayyaz-OdinSchool <fayyaz@odinschool.com>
2025-01-30 15:40:53 +0530

    FF ima

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import WindiCSS from 'vite-plugin-windicss';
 
 export default defineConfig({
-  plugins: [react(), WindiCSS()], // Removed PurgeCSS
+  plugins: [react()], // Removed WindiCSS & PurgeCSS
   base: '/portfolio/', // Ensure it's your GitHub repo name
   build: {
     minify: 'esbuild',
