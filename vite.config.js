ada83c62617f8c18a83fbabe86ce60ecc9c2339d
Sheikh-Fayyaz-OdinSchool <fayyaz@odinschool.com>
2025-01-30 15:53:11 +0530

    FF ima

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()], // Removed WindiCSS & PurgeCSS
+  plugins: [react()],
   base: '/portfolio/', // Ensure it's your GitHub repo name
-  build: {
-    minify: 'esbuild',
-    cssCodeSplit: true, // Splits CSS into smaller files
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        },
-      },
-    },
-  },
 });
