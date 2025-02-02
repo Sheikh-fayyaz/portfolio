be2ab53bc09eebb997624f5b3cf37c9f03b77040
sheikh fayyaz <fayyaz@zirafftechnologies.com>
2025-02-02 12:58:47 +0530

    Font add static

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import LoadCSS from "./components/LoadCSS.jsx"; // Import dynamically loading CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  <LoadCSS />
     <App />
   </StrictMode>,
 )
