9113c1d633071b7330fa7a59db3c6e24e2419f2d
sheikh fayyaz <fayyaz@zirafftechnologies.com>
2025-02-02 16:05:19 +0530

    removed postcss

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: './postcss.config.js', // Ensure Vite uses PostCSS correctly
-  },
   base: '/portfolio/', // Ensure it's correct for GitHub Pages deployment
   build: {
     minify: 'esbuild', // Faster minification than Terser
