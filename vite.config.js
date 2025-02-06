0b54cbf542ccf8dc75632432264a4ded5263d433
Sheikh-Fayyaz-OdinSchool <fayyaz@odinschool.com>
2025-02-06 17:01:07 +0530

    Auto Deployment

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,15 @@ export default defineConfig({
   plugins: [react()],
   base: '/portfolio/', // Ensure it's correct for GitHub Pages deployment
   build: {
-    minify: 'esbuild', // Faster minification than Terser
+    minify: 'esbuild', // Faster minification
+    assetsDir: 'assets', // Store static assets in 'assets' directory
+    manifest: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash][extname]', // Append hash to assets
+      },
+    },
   },
 });
