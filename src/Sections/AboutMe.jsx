d74bec612d853f2546f0472d90bbe769c26570ac
Sheikh-Fayyaz-OdinSchool <fayyaz@odinschool.com>
2025-02-25 17:41:50 +0530

    Auto Deployment

--- src/Sections/AboutMe.jsx
+++ src/Sections/AboutMe.jsx
@@ -6,13 +6,14 @@ export default function AboutMe() {
   return (
     <section className="about-me section-padY" id="about-me">
       <div className="container">
-      <div className="primary-bg p-md-5 p-4 rounded-4">
+      <div className="primary-bg p-md-5 p-4 rounded-4 aboutBg">
         <div className="row justify-content-end">
-          <div className="col-lg-7">
+          <div className="col-lg-7 col-md-7">
             <Title content="About Me"/>
             <Paragraph mb="mb-md-5 mb-4" content="I am a frontend developer with over 4+ years of experience creating responsive, user-friendly web solutions. Proficient in HTML, CSS, JavaScript, Bootstrap, and HubSpot CMS, I specialize in turning designs into functional, high-performance websites. With expertise in SEO, UI development, and modern frameworks like React.js and Sass, I deliver tailored digital experiences that meet client needs. " />
             <ContactButton/>
           </div>
+
         </div>
         </div>
       </div>
